0e1a6a564e048bd5882ce3f1d654bae209fac686
Konstantin <rebrum19200@gmail.com>
2019-06-15 16:51:12 +0300

    Add files via upload

--- scripts/best.js
+++ scripts/best.js
@@ -1,12 +1,17 @@
 var bestItem = document.querySelector(".main__best-pos");
 var bestVideo = document.querySelector(".main__best-iframe");
-var modalShadow= document.querySelector(".modal-shadow");
+var modalShadow = document.querySelector(".modal-shadow");
+var closeBtn = document.querySelector(".modal-shadow-close");
 
 bestItem.onclick = function(){
 	bestVideo.style.display = "block";
 	modalShadow.style.display = "block";
 }
 modalShadow.onclick = function(){
+	bestVideo.style.display = "none";
+	modalShadow.style.display = "none";
+}	
+closeBtn.onclick = function(){
 	bestVideo.style.display = "none";
 	modalShadow.style.display = "none";
 }	
